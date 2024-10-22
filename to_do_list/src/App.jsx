808765d380b68e2767185803a784def21ba40e27
zarif007008 <zarif007008az@gmail.com>
2024-10-22 23:20:26 +0600

    included components into app.jsx

--- to_do_list/src/App.jsx
+++ to_do_list/src/App.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react"
+import TodoForm from "./components/TodoForm"
+import TodoItem from "./components/TodoItem"
 import { TodoProvider } from "./context/TodoContext"
 
 function App() {
@@ -20,15 +22,16 @@ function App() {
     setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
  }
 
- useEffect = (() => {
+ useEffect(() => {
    const todos = JSON.parse(localStorage.getItem("todos"))
 
    if(todos && todos.length > 0){
       setTodos(todos)
    }
  }, [])
+ 
 
- useEffect = (() => {
+ useEffect(() => {
    localStorage.setItem("todos", JSON.stringify(todos))
  }, [todos])
 
@@ -39,9 +42,17 @@ function App() {
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoForm />
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => (
+                           <div key={todo.id}
+                           className="w-full"
+                           >
+                              <TodoItem todo={todo}/>
+                           </div>
+                        ))}
                     </div>
                 </div>
       </div>
